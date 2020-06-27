31db37945b632cf43b7e0b8d7632a9fec8dafd4a
konradgnat <konradmgnat@gmail.com>
2020-06-27 00:08:39 -0500

    add popup

---
+++ popup.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const getElemByID = id => document.getElementById(id);
+
+const button = getElemByID('dobSubmit');
+
+button.addEventListener('click', event => {
+  alert('clicked');
+});
+
+
