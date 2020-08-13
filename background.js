680f7277399dadc2a0e30ba81810fccba86a27ad
konradgnat <konradmgnat@gmail.com>
2020-08-13 00:06:54 -0500

    show popup;

--- background.js
+++ background.js
@@ -1,7 +1,17 @@
 'use strict';
 
+// todo: change to always show popup, independent of current url
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('hello world!');
+    console.log('The color is green.');
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
   });
 });
