50f1859d8f7433ce2843a2d6abbd36f8c8b11c02
konradgnat <konradmgnat@gmail.com>
2020-08-24 19:56:41 -0500

    set dob memory

--- popup.js
+++ popup.js
@@ -18,6 +18,12 @@ button.addEventListener('click', event => {
     CONTAINER.innerHTML = CONTAINER.innerHTML + '<h3>Please enter correct Date of birht format dd/mm/yyy</h3>';
     return;
   }
+  chrome.storage.sync.set({ 'dob': dob}, function() {
+  });
+});
+
+const displayResult = dob => {
+  const dobArr = dob.split('/');
   const dobArrInt = dobArr.map(d => parseInt(d));
   const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
   const dobDate = new Date(dobArrInt[2], dobArrInt[0], dobArrInt[1]);
@@ -25,6 +31,12 @@ button.addEventListener('click', event => {
   const today = new Date();
   const diffDays = Math.round(Math.abs((today - deathDate) / oneDay));
   CONTAINER.innerHTML = CONTAINER.innerHTML + `<h3>You have ${numberWithCommas(diffDays)} days left to live!</h3>`;
-});
+}
 
+window.onload = function() {
+  chrome.storage.sync.get(['dob'], function(result) {
+    alert('hi', result.key);
+    displayResult(result.key);
+  });
+}
 
