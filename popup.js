f063f117691406f6544a325d7e78a8e1c6f0af54
konradgnat <konradmgnat@gmail.com>
2020-08-28 20:07:04 -0500

    add storage, reset button

--- popup.js
+++ popup.js
@@ -3,23 +3,21 @@
 const getElemByID = id => document.getElementById(id);
 
 const button = getElemByID('dobSubmit');
+const CONTAINER = getElemByID('bodyContainer');
+const INPUT = getElemByID('dobInput');
 
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 button.addEventListener('click', event => {
-  const CONTAINER = getElemByID('bodyContainer');
-  const INPUT = getElemByID('dobInput');
-  CONTAINER.innerHTML = '';
   const dob = INPUT.value;
   const dobArr = dob.split('/');
   if (dobArr.length !== 3) {
-    CONTAINER.innerHTML = CONTAINER.innerHTML + '<h3>Please enter correct Date of birht format dd/mm/yyy</h3>';
+    CONTAINER.innerHTML = CONTAINER.innerHTML + '<h3>Please enter correct Date of birth format dd/mm/yyy</h3>';
     return;
   }
   chrome.storage.local.set({ dob: dob}, function() {
-    console.log('Value is set to ' + dob);
   });
 });
 
@@ -34,15 +32,18 @@ const displayResult = dob => {
   const deathDate = new Date(dobArrInt[2] + 85, dobArrInt[0], dobArrInt[1]);
   const today = new Date();
   const diffDays = Math.round(Math.abs((today - deathDate) / oneDay));
-  CONTAINER.innerHTML = CONTAINER.innerHTML + `<h3>You have ${numberWithCommas(diffDays)} days left to live!</h3>`;
+  CONTAINER.innerHTML = '';
+  CONTAINER.innerHTML = CONTAINER.innerHTML + `<div class="resultContainer"><h3>You have ${numberWithCommas(diffDays)} days left to live!</h3><p>You were born on: ${dob}. <a id="resetButton">reset</a></p></div>`;
+  const RESET_BUTTON = getElemByID('resetButton');
+  RESET_BUTTON.addEventListener('click', () => {
+    alert('reset clicked!');
+  })
 }
 
 window.onload = function() {
 
   chrome.storage.local.get(['dob'], function(result) {
-    console.log('Value currently is ' + result.dob);
     if (result.dob) {
-      alert('hi', result.dob);
       displayResult(result.dob);
     }
   });
